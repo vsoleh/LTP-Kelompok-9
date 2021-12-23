2aacd6ed6c8b39f469e1873b85291a169d525468
Fathi Muhammad Shalahuddin <sholachuddins@gmail.com>
2021-12-23 16:12:49 +0700

    Update KalkulatorBMI_prog.py

--- KalkulatorBMI_prog.py
+++ KalkulatorBMI_prog.py
@@ -1,24 +1,25 @@
 from KalkulatorBMI import *
 
+
 def signals(self):
-    self.PB_hitung.clicked.connect(self.hitungan)
+    self.pushButton.clicked.connect(self.hitungan)
 
 def hitungan(self):
     tb = float(self.Txt_tb.text())
     bb = float(self.Txt_bb.text())
     #hitung:
-    hasil= bb / tb 
-    self.label_hasil.setText(str(result))
+    result= bb / tb 
+    self.statusbar.setText(str(result))
 
-Ui_KalkulatorBMI.signals = signals
-Ui_KalkulatorBMI.hitungan = hitungan
+Ui_MainWindow.signals = signals
+Ui_MainWindow.hitungan = hitungan
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_KalkulatorBMI()
-    ui.setupUi(KalkulatorBMI)
-    ui.signals
-    KalkulatorBMI.show()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    ui.signals()
+    MainWindow.show()
     sys.exit(app.exec_())
