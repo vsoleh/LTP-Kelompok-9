80e8c539dcd3b168cff13f1594d7119d3d1c1fe0
fatafalihhilmi <94301313+fatafalihhilmi@users.noreply.github.com>
2021-12-29 13:10:42 +0700

    Update KalkulatorBmr_prog.py

--- KalkulatorBmr_prog.py
+++ KalkulatorBmr_prog.py
@@ -4,16 +4,18 @@ def signals(self):
     self.PB_hitung.clicked.connect(self.hitungan)
 
 def hitungan(self):
-    umur = float(self.Txt_umur.text())
-    kelamin = self.cmb_kelamin.currentText()
-    tb = float(self.Txt_tb.text())
-    bb = float(self.Txt_bb.text())
-    #try:
-    if (kelamin == "PRIA"):
-        result = 66.5+(13.7*bb)+(5.0*tb)-(6.0*umur)   
-    else :
-        result = 65.5 + (9.6 *bb) + (1.8 *tb) - (4.7 *umur)
-    self.label_hasil.setText(str(result))
+    try:
+        umur = float(self.Txt_umur.text())
+        kelamin = self.cmb_kelamin.currentText()
+        tb = float(self.Txt_tb.text())
+        bb = float(self.Txt_bb.text())
+        if (kelamin == "PRIA"):
+            result = 66.5+(13.7*bb)+(5.0*tb)-(6.0*umur) 
+        elif(kelamin == "WANITA"):
+            result = 655 + (9.6 *bb) + (1.8 *tb) - (4.7 *umur)
+        self.label_hasil.setText(str(result))
+    except:
+        self.label_hasil.setText(str('Error!!!'))
 
 Ui_KalkulatorBmr.signals = signals
 Ui_KalkulatorBmr.hitungan = hitungan
