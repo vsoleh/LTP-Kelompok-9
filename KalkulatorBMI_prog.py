a0d08254a0660223a44e2bfab34c4c9d0aa2d4b5
Fathi Muhammad Shalahuddin <sholachuddins@gmail.com>
2021-12-31 08:10:56 +0700

    Update KalkulatorBMI_prog.py

--- KalkulatorBMI_prog.py
+++ KalkulatorBMI_prog.py
@@ -4,23 +4,28 @@ from KalkulatorBMI import *
 def signals(self):
     self.pb_hitung.clicked.connect(self.hitungan)
 
+
 def hitungan(self):
     try:
         tb = float(self.Txt_tb.text())
         bb = float(self.Txt_bb.text())
         result= bb / (tb/100)**2
-        if (18.5 <= result <= 25):
-            cek = "normal"
-        elif(result>25):
-            cek = "berat badan berlebih"
-        elif(result<18.5):
-            cek = "berat badan kurang"
+
+        if (result < 18.5):
+            cek = "Kategori : Berat Badan Kurang"
+        elif(result <= 25):
+            cek = "Kategori : Berat Badan Normal"
+        elif(result > 25):
+            cek = "Kategori : Berat Badan Berlebih" 
+
 
         self.label_hasil.setText("{:.2f} ".format(result))
         self.label_hasil2.setText(str(cek))
     
     except:
         self.label_hasil.setText(str('Error!!!'))
+
+
 
 Ui_MainWindow.signals = signals
 Ui_MainWindow.hitungan = hitungan
